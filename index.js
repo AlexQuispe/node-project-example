0d2e99c98d554cb65dd1851eb9d1bcf856b2f82b
Alex Quispe <q.wilmer.alex@gmail.com>
2020-06-05 16:36:22 -0400

    express app added

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hello world!!!');
+const express    = require('express')
+const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT     || 4000
+const ENV  = process.env.NODE_ENV || 'development'
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res, next) => {
+  const result = {
+    msg : 'ok',
+    env : ENV,
+  }
+  return res.json(result)
+})
+
+app.listen(PORT, () => {
+  console.log('App running on port', PORT)
+})
+
+module.exports = app
